563c1a4cedea82fb237e6d6ebf78f4b523f1449e
rain1238888 <3172659938@qq.com>
2022-11-10 10:51:43 +0800

    first commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+// array of Monitor-specific API keys or Read-Only API keys to list all monitors
+// https://uptimerobot.com/dashboard.php#mySettings
+var __apiKeys = [
+ 
+    'ur1905090-d5d3541309c6f9b4acc54f5b'       // Read-Only API Key
+
+];
+
+// refresh interval (in seconds)
+var __refresh = 300;
